46fffd98e09e5ffa8d4c460fba4cec67cffb4d5d
Filipuci <rafaellifipuci@outlook.com>
2025-06-23 20:50:38 -0300

    v1.4.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,85 @@
-async function pokemon() {
-
-  const pokemonUl = document.querySelector("#pokemon-list");
-
-  for (let i = 1; i <= 151; i++) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    const json = await response.json();
-
-    const pokeLi = document.createElement("li");
-    const pokeBox = document.createElement("div");
-    const pokeImg = document.createElement("img");
-    const pokeInfo = document.createElement("div");
-    const pokeNumberId = document.createElement("p");
-    const pokeName = document.createElement("div");
-    const pokeTypeList = document.createElement("ul");
-
-    pokeLi.classList.add("poke-card")
-    pokeBox.classList.add("poke-box")
-    pokeImg.classList.add("poke-img")
-    pokeImg.setAttribute("src", json.sprites.other["official-artwork"].front_default)
-    pokeInfo.classList.add("poke-info")
-    pokeNumberId.classList.add("poke-id")
-    pokeName.classList.add("poke-name")
-    pokeTypeList.classList.add("poke-type--list")
-
-    pokemonUl.append(pokeLi)
-    pokeLi.append(pokeBox)
-    pokeBox.append(pokeImg)
-    pokeBox.append(pokeInfo)
-    pokeInfo.append(pokeNumberId)
-    pokeInfo.append(pokeName)
-    pokeName.innerHTML = capitalize(json.name)
-    pokeInfo.append(pokeTypeList)
-
-    let paddedNumber = json.id.toString().padStart(4, '0')
-    pokeNumberId.innerHTML = `N° ${paddedNumber}`
-    
-    /* POKE-TYPE-LIST */
-
-    for (let { type } of json.types) {
-
-      const pokeType = document.createElement("li");
-      pokeType.innerHTML = capitalize(type.name);
-      pokeType.classList.add(type.name);
-      pokeTypeList.append(pokeType);
-
-    }
+async function fetchPokemon(id) {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    if (!res.ok) throw new Error('Falha ao buscar o pokemon')
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (err) {
+    return null
   }
 }
 
-pokemon();
+function renderPokemonImage(pokemon) {
+  const { name, id, sprites } = pokemon
 
-/* Letra maiúscula*/
+  if (!pokemon || !sprites.front_default) return
 
-const capitalize = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1, str.length);
+  const ul = document.querySelector('#pokemon-list')
+
+  const li = document.createElement('li')
+  li.className = 'poke-card'
+
+  const box = document.createElement('div')
+  box.className = 'poke-box'
+
+  const img = document.createElement('img')
+  img.src = sprites.other["official-artwork"].front_default
+  img.alt = name
+
+  const info = document.createElement('div')
+  info.className = 'poke-info'
+
+  const pokeId = document.createElement('p')
+  pokeId.className = 'poke-id'
+  pokeId.textContent = `N° ${padding(id)}`
+
+  const pokeName = document.createElement('p')
+  pokeName.className = 'poke-name'
+  pokeName.textContent = capitalize(name)
+
+  const pokemonTypeList = document.createElement('ul')
+  pokemonTypeList.className = 'pokemonType-list'
+
+  const pokemonType = document.createElement('li')
+  pokemonType.classList.add('pokemon-type', `${pokemon.types[0].type.name}`)
+  pokemonType.textContent = capitalize(pokemon.types[0].type.name)
+
+  
+  li.appendChild(box)
+  box.appendChild(img)
+  box.appendChild(info)
+  info.appendChild(pokeId)
+  info.appendChild(pokeName)
+  info.appendChild(pokemonTypeList)
+  pokemonTypeList.appendChild(pokemonType)
+  ul.appendChild(li)
+  
+  if (pokemon.types[1]) {
+    const pokemonSecondType = document.createElement('li')
+    pokemonSecondType.classList.add('pokemon-type', `${pokemon.types[1].type.name}`)
+    pokemonSecondType.textContent = capitalize(pokemon.types[1].type.name)
+    pokemonTypeList.appendChild(pokemonSecondType)
+  }
 }
+
+function capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1)
+}
+
+function padding(id) {
+  return id.toString().padStart(4, '0')
+}
+
+async function loadPokemons() {
+  const promises = []
+
+  for (let i = 1; i <= 151; i++) {
+    promises.push(fetchPokemon(i))
+  }
+
+  const pokemons = await Promise.all(promises)
+  pokemons.forEach(renderPokemonImage)
+}
+
+loadPokemons()
